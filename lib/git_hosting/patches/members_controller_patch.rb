f76646d86c3bd021a234911f9f00f3b5cc889eff
Déja <deja@libertyconcepts.com>
2012-08-01 12:38:22 -0400

    Hm… this may be more involved

--- lib/git_hosting/patches/members_controller_patch.rb
+++ lib/git_hosting/patches/members_controller_patch.rb
@@ -7,31 +7,31 @@ module GitHosting
       def self.included(base)
       	base.send(:include, InstanceMethods)
       	base.class_eval do
-      		alias_method_chain :new, :disable_update
-          alias_method_chain :edit, :disable_update
+      		alias_method_chain :create, :disable_update
+          alias_method_chain :update, :disable_update
           alias_method_chain :destroy, :disable_update
         end
       end
 
       module InstanceMethods
-        def new_with_disable_update
+        def create_with_disable_update
           Rails.logger.info("new_with_disable_update")
           # Turn of updates during repository update
        		GitHostingObserver.set_update_active(false);
 
        		# Do actual update
-       		new_without_disable_update
+       		create_without_disable_update
 
        		# Reenable updates to perform a single update
           GitHostingObserver.set_update_active(true);
        	end
-        def edit_with_disable_update
+        def update_with_disable_update
           Rails.logger.info("edit_with_disable_update")
              	# Turn of updates during repository update
        		GitHostingObserver.set_update_active(false);
 
        		# Do actual update
-       		edit_without_disable_update
+       		update_without_disable_update
 
        		# Reenable updates to perform a single update
           GitHostingObserver.set_update_active(true);
