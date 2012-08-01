689747f72f81409ff1fe932f9aad930d6983a5b3
Déja <deja@libertyconcepts.com>
2012-08-01 12:36:10 -0400

    Do I need to update all the patches like this?

--- lib/git_hosting/patches/members_controller_patch.rb
+++ lib/git_hosting/patches/members_controller_patch.rb
@@ -2,50 +2,54 @@
 
 module GitHosting
 	module Patches
-		module MembersControllerPatch
-                        def new_with_disable_update
-                          Rails.logger.info("new_with_disable_update")
-                             	# Turn of updates during repository update
-                       		GitHostingObserver.set_update_active(false);
+		module MembersControllerPatch         
+      
+      def self.included(base)
+      	base.send(:include, InstanceMethods)
+      	base.class_eval do
+      		alias_method_chain :new, :disable_update
+          alias_method_chain :edit, :disable_update
+          alias_method_chain :destroy, :disable_update
+        end
+      end
 
-                       		# Do actual update
-                       		new_without_disable_update
+      module InstanceMethods
+        def new_with_disable_update
+          Rails.logger.info("new_with_disable_update")
+          # Turn of updates during repository update
+       		GitHostingObserver.set_update_active(false);
 
-                       		# Reenable updates to perform a single update
-				GitHostingObserver.set_update_active(true);
-                       	end
-                        def edit_with_disable_update
-                             	# Turn of updates during repository update
-                       		GitHostingObserver.set_update_active(false);
+       		# Do actual update
+       		new_without_disable_update
 
-                       		# Do actual update
-                       		edit_without_disable_update
+       		# Reenable updates to perform a single update
+          GitHostingObserver.set_update_active(true);
+       	end
+        def edit_with_disable_update
+          Rails.logger.info("edit_with_disable_update")
+             	# Turn of updates during repository update
+       		GitHostingObserver.set_update_active(false);
 
-                       		# Reenable updates to perform a single update
-				GitHostingObserver.set_update_active(true);
-                       	end
-                        def destroy_with_disable_update
-                             	# Turn of updates during repository update
-                       		GitHostingObserver.set_update_active(false);
+       		# Do actual update
+       		edit_without_disable_update
 
-                       		# Do actual update
-                       		destroy_without_disable_update
+       		# Reenable updates to perform a single update
+          GitHostingObserver.set_update_active(true);
+       	end
+        def destroy_with_disable_update
+          Rails.logger.info("destroy_with_disable_update")
+             	# Turn of updates during repository update
+       		GitHostingObserver.set_update_active(false);
 
-                       		# Reenable updates to perform a single update
-				GitHostingObserver.set_update_active(:delete => true);
-                       	end
+       		# Do actual update
+       		destroy_without_disable_update
 
-			def self.included(base)
-				base.class_eval do
-					unloadable
-				end
-                        	begin
-                                	base.send(:alias_method_chain, :new, :disable_update)
-                                	base.send(:alias_method_chain, :edit, :disable_update)
-                                	base.send(:alias_method_chain, :destroy, :disable_update)
-                                rescue
-                                end
-			end
+       		# Reenable updates to perform a single update
+          GitHostingObserver.set_update_active(:delete => true);
+       	end
+      	
+      end
+      
 		end
 	end
 end
